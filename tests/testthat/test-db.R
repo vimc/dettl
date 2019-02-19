d95c8df049d761495491ff85414e2b29a7fb8999
Robert Ashton <r.ashton@imperial.ac.uk>
2019-02-19 14:14:05 +0000

    Add test for connecting to remote DB

--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -8,5 +8,11 @@ test_that("db can connect to database using yaml config", {
 
   con <- db_connect("destination", ".")
   expect_true(DBI::dbIsValid(con))
+})
 
+test_that("dettl can connect to remote DB using yaml config", {
+  con <- db_connect("source", "uat_config")
+  on.exit(DBI::dbDisconnect(con))
+  expect_true(DBI::dbIsValid(con))
+  expect_true(length(DBI::dbListTables(con)) > 0)
 })
