10e9d7abc0fb93507a7ce48c17e9c217d346390c
Robert Ashton <r.ashton@imperial.ac.uk>
2019-02-20 15:37:22 +0000

    Update test to copy cfg to temporary files instead of overwriting existing

--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -14,21 +14,16 @@ test_that("dettl can connect to remote DB using yaml config and the vault", {
   cl <- srv$client()
   cl$write("/secret/users/readonly", list(password = "readonly"))
 
-  ## Ensure addr to vault is correct for this particular test run
-  test_cfg <- "uat_config/db_config.yml"
-  cfg <- readLines(test_cfg)
-  expect_true("vault_server: <vault_server>" %in% cfg, sprintf(
-    "Vault server template not found in test config %s, test misconfigured and
-will fail. Add 'vault_server: <vault_server> to cfg to be overriden by server
-URL for individual test. ", test_cfg
-  ))
+  path <- tempfile()
+  dir.create(path)
+  filename <- file.path(path, "db_config.yml")
+  cfg <- readLines("uat_config/db_config.yml")
   cfg_server <- gsub(pattern = "<vault_server>", replace = srv$addr, x = cfg)
-  on.exit(writeLines(cfg, con = "uat_config/db_config.yml"), add = TRUE)
-  writeLines(cfg_server, con = "uat_config/db_config.yml")
+  writeLines(cfg_server, filename)
 
   withr::with_envvar(c(VAULTR_AUTH_METHOD = "token", VAULT_TOKEN = srv$token), {
-    con <- db_connect("source", "uat_config")
-    on.exit(DBI::dbDisconnect(con), add = TRUE)
+    con <- db_connect("source", path)
+    on.exit(DBI::dbDisconnect(con))
     expect_true(DBI::dbIsValid(con))
     expect_true(length(DBI::dbListTables(con)) > 0)
   })
