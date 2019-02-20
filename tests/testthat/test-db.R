aabe320fd0b9441c9a9653264098f3ed0feb2b4d
Robert Ashton <r.ashton@imperial.ac.uk>
2019-02-20 14:39:10 +0000

    Fix test regression and add useful message shown if regression occurs again

--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -1,7 +1,6 @@
 context("test-db")
 
 test_that("db can connect to database using yaml config", {
-
   db_name <- "test.sqlite"
   prepare_example_db(db_name)
   on.exit(unlink(db_name))
@@ -15,17 +14,22 @@ test_that("dettl can connect to remote DB using yaml config and the vault", {
   cl <- srv$client()
   cl$write("/secret/users/readonly", list(password = "readonly"))
 
-  ## Ensure addr to vault is correct for cfg
-  cfg <- readLines("uat_config/db_config.yml")
+  ## Ensure addr to vault is correct for this particular test run
+  test_cfg <- "uat_config/db_config.yml"
+  cfg <- readLines(test_cfg)
+  expect_true("vault_server: <vault_server>" %in% cfg, sprintf(
+    "Vault server template not found in test config %s, test misconfigured and
+will fail. Add 'vault_server: <vault_server> to cfg to be overriden by server
+URL for individual test. ", test_cfg
+  ))
   cfg_server <- gsub(pattern = "<vault_server>", replace = srv$addr, x = cfg)
   writeLines(cfg_server, con = "uat_config/db_config.yml")
-  on.exit(writeLines(cfg, con = "uat_config/db_config.yml"))
+  on.exit(writeLines(cfg, con = "uat_config/db_config.yml"), add = TRUE)
 
   withr::with_envvar(c(VAULTR_AUTH_METHOD = "token", VAULT_TOKEN = srv$token), {
     con <- db_connect("source", "uat_config")
-    on.exit(DBI::dbDisconnect(con))
+    on.exit(DBI::dbDisconnect(con), add = TRUE)
     expect_true(DBI::dbIsValid(con))
     expect_true(length(DBI::dbListTables(con)) > 0)
   })
 })
-
