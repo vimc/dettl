37bf164d72b96b704375bb534fdfa8a639099903
Robert Ashton <r.ashton@imperial.ac.uk>
2019-02-26 10:02:26 +0000

    Fix typo in template import

--- inst/template/R/test_transform.R
+++ inst/template/R/test_transform.R
@@ -1,6 +1,6 @@
 context("transform")
 
-testthat::test_file("transformed data is as expected", {
+testthat::test_that("transformed data is as expected", {
   ## Example impl, extend or modify as required.
   expect_false(is.null(transformed_data))
 })
