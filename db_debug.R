bd514ffc5d286bb339559d3735d4dbb15ac4106b
Robert Ashton <r.ashton@imperial.ac.uk>
2019-04-12 16:16:05 +0100

    Try the postgres connection first

--- db_debug.R
+++ db_debug.R
@@ -1,5 +1,12 @@
 options(error = traceback)
 
+dbname <- "dettl_test_db"
+user <- "postgres"
+host <- "localhost"
+con_pg <- DBI::dbConnect(RPostgres::Postgres(), dbname = dbname, user = user,
+                         host = host)
+DBI::dbDisconnect(con_pg)
+
 con_sqlite <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
 DBI::dbDisconnect(con_sqlite)
 
