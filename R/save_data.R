687da9defa6374d092eacb7d6c6bb79b367cc51f
Robert Ashton <r.ashton@imperial.ac.uk>
2019-04-15 15:12:36 +0100

    Add function to save extracted or transformed data as xlsx

---
+++ R/save_data.R
@@ -0,0 +1,59 @@
+#' Save extracted data
+#'
+#' Saves any extracted data as separate sheet of an xlsx file.
+#'
+#' @param import The import object to save the extracted data for.
+#' @param file Optional file to save the data to, defaults to creating file in
+#' current working directory called extracted_data.xlsx
+#'
+#' @export
+#'
+#' @examples
+#' import <- new_import(system.file("examples/person_information", package = "dettl"), "test")
+#' run_import(import, "extract")
+#' t <- tempfile()
+#' save_extracted_data(import, t)
+save_extracted_data <- function(import, file = "extracted_data.xlsx") {
+  save_data(import, file, "extracted")
+}
+
+#' Save transformed data
+#'
+#' Saves any transformed data as separate sheet of an xlsx file.
+#'
+#' @param import The import object to save the transformed data for.
+#' @param file Optional file to save the data to, defaults to creating file in
+#' current working directory called transformed_data.xlsx
+#'
+#' @export
+#'
+#' @examples
+#' import <- new_import(system.file("examples/person_information", package = "dettl"), "test")
+#' run_import(import, c("extract", "transform))
+#' t <- tempfile()
+#' save_transformed_data(import, t)
+save_transformed_data <- function(import, file = "transformed_data.xlsx") {
+  save_data(import, file, "transformed")
+}
+
+save_data <- function(import, file, stage) {
+  if (!inherits(import, "dataImport")) {
+    stop(sprintf("Can't save %s data for non-DataImport object.", stage))
+  }
+  if (!file.exists(file)) {
+    file.create(file)
+  }
+
+  if (stage == "extracted") {
+    data <- import$get_extracted_data()
+  } else if (stage == "transformed") {
+    data <- import$get_transformed_data()
+  }
+
+  if (!is.null(data)) {
+    writexl::write_xlsx(data, file)
+  } else {
+    stop(sprintf("Can't save %s data as stage has not been run.", stage))
+  }
+  invisible(file)
+}
---
+++ tests/testthat/test-save-data.R
@@ -0,0 +1,83 @@
+context("save-data")
+
+test_that("extracted data can be saved", {
+  db_name <- "test.sqlite"
+  prepare_example_db(db_name)
+  on.exit(unlink(db_name), add = TRUE)
+
+  ## Turn off reporting when running import so import tests do not print
+  ## to avoid cluttering up test output.
+  default_reporter <- testthat::default_reporter()
+  options(testthat.default_reporter = "silent")
+  on.exit(options(testthat.default_reporter = default_reporter), add = TRUE)
+
+  import <- new_import("example/", "test")
+
+  file <- tempfile()
+  expect_error(save_extracted_data(import, file),
+               "Can't save extracted data as stage has not been run.")
+
+  run_import(import, "extract")
+  save_extracted_data(import, file)
+
+  expect_equal(readxl::excel_sheets(file), "people")
+  xl_data <- readxl::read_excel(file, sheet = "people")
+  expect_equal(colnames(xl_data), c("name", "age", "height"))
+  expect_equal(nrow(xl_data), 3)
+})
+
+test_that("transformed data can be saved", {
+  db_name <- "test.sqlite"
+  prepare_example_db(db_name)
+  on.exit(unlink(db_name), add = TRUE)
+
+  ## Turn off reporting when running import so import tests do not print
+  ## to avoid cluttering up test output.
+  default_reporter <- testthat::default_reporter()
+  options(testthat.default_reporter = "silent")
+  on.exit(options(testthat.default_reporter = default_reporter), add = TRUE)
+
+  import <- new_import("example/", "test")
+
+  file <- tempfile()
+  expect_error(save_transformed_data(import, file),
+    "Can't save transformed data as stage has not been run.")
+
+  run_import(import, c("extract", "transform"))
+  save_transformed_data(import, file)
+
+  expect_equal(readxl::excel_sheets(file), "people")
+  xl_data <- readxl::read_excel(file, sheet = "people")
+  expect_equal(colnames(xl_data), c("name", "age", "height"))
+  expect_equal(nrow(xl_data), 2)
+})
+
+test_that("trying to save data with non data import object fails", {
+  expect_error(save_transformed_data("test"),
+               "Can't save transformed data for non-DataImport object.")
+})
+
+test_that("save data can create new file and save to default location", {
+  db_name <- "test.sqlite"
+  prepare_example_db(db_name)
+  on.exit(unlink(db_name), add = TRUE)
+
+  ## Turn off reporting when running import so import tests do not print
+  ## to avoid cluttering up test output.
+  default_reporter <- testthat::default_reporter()
+  options(testthat.default_reporter = "silent")
+  on.exit(options(testthat.default_reporter = default_reporter), add = TRUE)
+
+  import <- new_import("example/", "test")
+  run_import(import, "extract")
+
+  dir <- tempdir()
+  file <- file.path(dir, "test.xlsx")
+  save_extracted_data(import, file)
+  expect_true("test.xlsx" %in% list.files(dir))
+
+  withr::with_dir(dir, {
+    save_extracted_data(import)
+    expect_true("extracted_data.xlsx" %in% list.files())
+  })
+})
