c122b2ec1640523fc3659473e62b8126e9dc1911
Robert Ashton <r.ashton@imperial.ac.uk>
2019-05-21 18:13:16 +0100

    Add coverage to mimic incorrect file case on windows systems

--- tests/testthat/test-utils-assert.R
+++ tests/testthat/test-utils-assert.R
@@ -42,6 +42,25 @@ test_that("assert_file_exists", {
   expect_silent(assert_file_exists(path))
 })
 
+test_that("assert_file_exists works with incorrect case", {
+  ## Note this would only ever work on windows or mac as linux filesystems
+  ## are case-sensitive. Therefore to test this requires some mocking to
+  ## mimic behaviour as windows would run it.
+  mock_is_linux <- mockery::mock(FALSE, cycle = TRUE)
+  mock_file_has_canonical_case <- mockery::mock(FALSE, cycle = TRUE)
+  path <- tempfile()
+  writeLines(character(0), path)
+  with_mock(
+    "dettl:::is_linux" = mock_is_linux,
+    "dettl:::file_has_canonical_case" = mock_file_has_canonical_case,
+    {
+      expect_error(
+        assert_file_exists(path),
+        sprintf("File does not exist: '%s' \\(should be '%s'\\) in directory .",
+                path, path))
+    })
+})
+
 test_that("assert_named", {
   expect_error(assert_named(1), "must be named")
   expect_error(assert_named(setNames(1:2, c("a", "a")), TRUE),
