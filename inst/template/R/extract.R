592763287937d8d420e222a36db421b47fce7c62
Robert Ashton <r.ashton@imperial.ac.uk>
2020-02-11 16:09:18 +0000

    Add path note to import template docs

--- inst/template/R/extract.R
+++ inst/template/R/extract.R
@@ -1,7 +1,8 @@
 #' Extract data from sources.
 #'
 #' This step should pull data from sources -  local files or database. And load
-#' it into memory ready for transform stage.
+#' it into memory ready for transform stage. Paths should all be written
+#' relative to the root of the import directory.
 #'
 #' @param con The active DBI connection for extracting any data.
 #'
