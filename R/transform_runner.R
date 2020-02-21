1bb999e8bbda98d75d6e6dcd568dc2e1a1528cd1
Robert Ashton <r.ashton@imperial.ac.uk>
2020-02-21 16:22:27 +0000

    Fold tests into withr block

--- R/transform_runner.R
+++ R/transform_runner.R
@@ -22,19 +22,20 @@ run_transform <- function(con, transform, path, extracted_data,
     }
     withr::with_dir(path, {
       transformed_data <- transform(extracted_data)
-    })
-    verify_data(con, transformed_data, mode)
-    if (!is.null(transform_test)) {
-      message(sprintf("Running transform tests %s", transform_test))
-      test_path <- file.path(path, transform_test)
-      test_results <-
-        run_transform_tests(test_path, transformed_data, extracted_data, con)
-      if (!all_passed(test_results)) {
-        stop("Not all transform tests passed. Fix tests before proceeding.")
-      } else {
-        message("All transform tests passed.")
+
+      verify_data(con, transformed_data, mode)
+      if (!is.null(transform_test)) {
+        message(sprintf("Running transform tests %s", transform_test))
+        test_path <- file.path(path, transform_test)
+        test_results <-
+          run_transform_tests(test_path, transformed_data, extracted_data, con)
+        if (!all_passed(test_results)) {
+          stop("Not all transform tests passed. Fix tests before proceeding.")
+        } else {
+          message("All transform tests passed.")
+        }
       }
-    }
+    })
     transformed_data
   }
 
