1d53d29bcc9827fa31a44f34ff3d63cae256625b
Robert Ashton <r.ashton@imperial.ac.uk>
2020-02-28 11:23:05 +0000

    Update template upload to match current expected usage

---
+++ inst/template/R/verification_queries.R
@@ -0,0 +1,5 @@
+verification_queries <- function(con) {
+  ## Return a list of data extracted from the DB.
+  ## This will get run before and after the load stage and made available
+  ## to the load tests so you can check for the differences made by load stage
+}
