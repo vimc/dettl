63a814eede4c0eddc02014600c96cb7b4ab314ff
Robert Ashton <r.ashton@imperial.ac.uk>
2020-11-03 10:31:22 +0000

    Add script to trigger error when failing to connect to postgres

---
+++ scripts/tmp_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+## Connect to postgres db
+
+con <- DBI::dbConnect(RPostgres::Postgres(),
+                      dbname = "dettl_test_db",
+                      user = "postgres",
+                      host = "localhost")
+DBI::dbDisconnect(con)
+
+## Connect to vault
+#vaultr::vault_resolve_secrets()
